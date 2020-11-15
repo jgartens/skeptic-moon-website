e7d2c5d45d233ba3ee44e9ef56f684080574bfd0
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-15 13:39:49 -0600

    init commit

---
+++ src/main/java/com/tts/skepticmoon/SkepticmoonApplication.java
@@ -0,0 +1,13 @@
+package com.tts.skepticmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkepticmoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkepticmoonApplication.class, args);
+	}
+
+}
