c162773e535c8ffc8686fdb389303eb4b74788a6
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-21 17:23:27 -0600

    bug fixes

--- src/main/java/com/tts/skepticmoon/models/User.java
+++ src/main/java/com/tts/skepticmoon/models/User.java
@@ -1,9 +1,11 @@
 package com.tts.skepticmoon.models;
 
 
+import java.beans.JavaBean;
+
+import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-
 
 public class User {
     
