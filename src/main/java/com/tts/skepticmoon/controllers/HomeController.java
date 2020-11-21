210f2e091042ade45fb178438f48dce0f8ca1515
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-21 16:34:14 -0600

    Added post mapping for contact form

--- src/main/java/com/tts/skepticmoon/controllers/HomeController.java
+++ src/main/java/com/tts/skepticmoon/controllers/HomeController.java
@@ -1,7 +1,14 @@
 package com.tts.skepticmoon.controllers;
 
+import javax.validation.Valid;
+
+import com.tts.skepticmoon.models.User;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -32,6 +39,19 @@ public class HomeController {
 
     @GetMapping("/contact")
     public String contact(){
+        return "/contact";
+    }
+
+    @PostMapping("/contact")
+    public String formPost(@Valid User user, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("errorMsg", "Input is not valid. Please re-enter and re-submit");
+            return "/contact";
+        }
+
+        model.addAttribute("noErrors", true);
+        model.addAttribute("user", user);
+
         return "/contact";
     }
 }
