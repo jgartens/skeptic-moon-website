5968326712a97a53db256f2f97b573ba778dee3b
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-21 21:15:47 -0600

    added annotation

--- src/main/java/com/tts/skepticmoon/repository/BlogPostRepository.java
+++ src/main/java/com/tts/skepticmoon/repository/BlogPostRepository.java
@@ -1,9 +1,12 @@
 package com.tts.skepticmoon.repository;
 
+import java.util.List;
+
 import com.tts.skepticmoon.models.BlogPost;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
-    
 }
